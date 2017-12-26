d5b768149425d796aaf45d05bf2fe28756c469b3
Dawid <dawidpol1@gmail.com>
2017-12-26 15:50:17 +0800

    Bug fix: empty error response

--- src/errors.js
+++ src/errors.js
@@ -1,14 +1,11 @@
 export function TransactionPending() {
-  console.log('Blah TransactionPending')
 }
 TransactionPending.prototype = new Error('Transaction pending');
 
 export function TransactionFailed() {
-  console.log('Blah TransactionFailed')
 }
 TransactionFailed.prototype = new Error('Transaction failed');
 
 export function SignatureNotMatching() {
-  console.log('Blah SignatureNotMatching')
 }
 SignatureNotMatching.prototype = new Error('Signature not matching');
--- test/response.spec.js
+++ test/response.spec.js
@@ -49,7 +49,7 @@ describe('Response handling', () => {
       ).to.throw(TransactionFailed)
     })
     it('should throw exception TransactionPending', () => {
-      let response = 'TransactionType=SALE&PymtMethod=DD&ServiceID=sit&PaymentID=49f29e98c1cba9d42c37&OrderNumber=42a0a717b9dddfd82cf3&Amount=0.14&CurrencyCode=MYR&HashValue=6494a24c8d2b9f2021f7c680c774af23d1b3b9b415da959422ad50fea5f4277d&HashValue2=04ab06298491b70b093b5583f83aadc805ddfa53e007b414520c81389ee398cc&TxnID=sit49f29e98c1cba9d42c37&IssuingBank=HostSim&TxnStatus=1&AuthCode=&BankRefNo=sit49f29e98c1cba9d42c37&RespTime=2017-12-26+10%3A37%3A53&TxnMessage=Transaction+Failed'
+      let response = 'TransactionType=SALE&PymtMethod=DD&ServiceID=sit&PaymentID=49f29e98c1cba9d42c37&OrderNumber=42a0a717b9dddfd82cf3&Amount=0.14&CurrencyCode=MYR&HashValue=6494a24c8d2b9f2021f7c680c774af23d1b3b9b415da959422ad50fea5f4277d&HashValue2=199b35e86da9a678acdaa3cebff3ba2d414fc206250ed27440129c36894a615a&TxnID=sit49f29e98c1cba9d42c37&IssuingBank=HostSim&TxnStatus=2&AuthCode=&BankRefNo=sit49f29e98c1cba9d42c37&RespTime=2017-12-26+10%3A37%3A53&TxnMessage=Transaction+Pending'
 
       expect(
         () => validateAndProcessPaymentResponse(response, MERCHANT_PASSWORD)
